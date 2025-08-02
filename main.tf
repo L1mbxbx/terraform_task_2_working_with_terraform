136590077beeea24d977041394f5e7ce85de5413
L1mbxbx <ilya.msd2003@gmail.com>
2025-08-02 18:28:49 +0300

    Fix

--- main.tf
+++ main.tf
@@ -26,7 +26,7 @@ resource "azurerm_storage_container" "example" {
   container_access_type = "private"
 }
 
-resource "azurerm_storage_blob" "name" {
+resource "azurerm_storage_blob" "example" {
   name                   = var.blob_name
   storage_account_name   = azurerm_storage_account.example.name
   storage_container_name = azurerm_storage_container.example.name
