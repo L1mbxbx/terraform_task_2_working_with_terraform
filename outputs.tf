a6ca88a505263a8d4e90b4c69d3478c0c4dd1d13
L1mbxbx <ilya.msd2003@gmail.com>
2025-08-02 18:34:18 +0300

    Fix

--- outputs.tf
+++ outputs.tf
@@ -1,8 +1,8 @@
 output "blob_id" {
-  value     = azurerm_storage_blob.name.id
+  value     = azurerm_storage_blob.example.id
   sensitive = true
 }
 
 output "blob_url" {
-  value = azurerm_storage_blob.name.url
+  value = azurerm_storage_blob.example.url
 }
